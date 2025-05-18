64c5851915e5044c5ecccb485aef17f1eb7d0a6d
qtalen <qtalen@gmail.com>
2025-05-18 20:34:05 +0800

    Prepare source code for my new article.

--- 03_Master_AutoGen-Qwen3_Integration/01_openai_like_client.py
+++ 03_Master_AutoGen-Qwen3_Integration/01_openai_like_client.py
@@ -3,10 +3,15 @@ import asyncio
 from dotenv import load_dotenv
 from autogen_agentchat.agents import AssistantAgent
 from autogen_agentchat.ui import Console
+from autogen_ext.models.openai import OpenAIChatCompletionClient
 
 from utils.openai_like import OpenAILikeChatCompletionClient
 
 load_dotenv("../.env")
+
+original_model_client = OpenAIChatCompletionClient(
+    model="qwen-plus-latest"
+)
 
 model_client = OpenAILikeChatCompletionClient(
     model="qwen-plus-latest"
@@ -14,6 +19,7 @@ model_client = OpenAILikeChatCompletionClient(
 
 agent = AssistantAgent(
     name="assistant",
+    # model_client=original_model_client,
     model_client=model_client,
     system_message="You are a helpful assistant."
 )
